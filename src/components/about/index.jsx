f4d87ef0a0050ad637d99ba84d20c2941db3c01d
Justin Berg <justinberg@Justins-MBP.localdomain>
2021-10-25 20:05:07 -0600

    JSX files

---
+++ src/components/about/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+function About () {
+
+    return(
+        <section id="aboutme">
+            <div className="about-container">
+                <div className="title-header">About Me</div>
+                <div className="about-section">
+                    <div className="img-container">
+                        <div className="about-image"></div>
+                    </div>
+                    <div className="about-text">
+                        I am Justin Berg and I 
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About;
